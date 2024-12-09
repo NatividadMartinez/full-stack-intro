dfc807f754b0ff71cc5f9c89f78f0d6d2c6a875a
Nati Martinez <natividad.martinez@reutlingen-university.de>
2024-12-09 01:47:29 +0100

    some changes to the code

---
+++ app/page.jsx
@@ -0,0 +1,39 @@
+import Post from "./components/Post";
+import styles from "./page.module.css";
+import prisma from "@/lib/prisma";
+
+async function getPosts(params) {
+  const posts = await prisma.post.findMany({
+    where: {published: true},
+    include: {
+      author: {
+        select: {name: true}
+      }
+    }
+  })
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts()
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Feed</h1>
+       {
+        posts.map((post) => {
+          return (
+            <Post 
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              authorName={post.author.name}
+            > </Post>
+          )
+        })
+      } 
+      </main>
+    </div>
+  );
+}
